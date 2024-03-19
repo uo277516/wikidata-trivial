234537ed29295dd043c0a7551593a4372237288c
uo277516 <90771958+uo277516@users.noreply.github.com>
2024-03-19 02:12:54 +0100

    añadido paquete editar, no configurado

--- back-trivial/repositories/researchersRepository.js
+++ back-trivial/repositories/researchersRepository.js
@@ -1,12 +1,52 @@
 const WBK = require('wikibase-sdk');
+const WBEdit = require('wikibase-edit');
+
 const axios = require('axios');
 const transformJSON = require('../utils/transformJSON');
-
 
 const wdk = WBK({
   instance: 'https://www.wikidata.org',
   sparqlEndpoint: 'https://query.wikidata.org/sparql'
 });
+
+
+
+const generalConfig = {
+  // A Wikibase instance is required
+  instance: 'https://www.wikidata.org',
+
+  // The instance script path, used to find the API endpoint
+  // Default: /w
+  wgScriptPath: '/w',
+
+  // One authorization mean is required (unless in anonymous mode, see below)
+  credentials: {
+    // either a username and password
+    username: 'my-wikidata-username',
+    // Optional: generate a dedicated password with tailored rights on /wiki/Special:BotPasswords
+    // See the 'Credentials' paragraph below
+    password: 'my-wikidata-password',
+
+    // OR OAuth tokens
+    oauth: {
+      // Obtained at registration
+      // https://www.mediawiki.org/wiki/OAuth/For_Developers#Registration
+      consumer_key: 'your-consumer-token',
+      consumer_secret: 'your-secret-token',
+      // Obtained when the user authorized your service
+      // see https://www.mediawiki.org/wiki/OAuth/For_Developers#Authorization
+      token: 'a-user-token',
+      token_secret: 'a-secret-token'
+    }
+  }
+};
+
+const wbEdit = WBEdit(generalConfig)
+
+
+
+
+
 
 const researchersRepository = {
   getResearchers: async () => {
