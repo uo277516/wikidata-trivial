e5ccd6cd654cd0b8d5f6b8098cdf0303995fa43b
uo277516 <90771958+uo277516@users.noreply.github.com>
2024-04-02 20:11:48 +0200

    Header hecho

--- front-trivial/src/App.js
+++ front-trivial/src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+import {Layout, Flex, Typography, Image} from 'antd';
+import logo from './logo.png'; 
+import React from 'react';
 
-function App() {
+
+let App = () => {
+
+
+  const {Text, Title} = Typography;
+  const { Header, Footer, Sider, Content } = Layout;
+
+  const headerStyle = {
+    textAlign: 'center',
+    color: 'white',
+    height: 250,
+    paddingInline: 0,
+    lineHeight: '64px',
+    backgroundColor: 'white'
+  };
+  const contentStyle = {
+    textAlign: 'left',
+    paddingLeft: '20px',
+    minHeight: 120,
+    lineHeight: '120px',
+    color: 'black',
+    backgroundColor: '#B0E7F8',
+  };
+  const headerRightStyle = {
+    textAlign: 'left',
+    paddingLeft: '0px',
+    minHeight: 120,
+    lineHeight: '120px',
+    color: 'black',
+    backgroundColor: 'white',
+    paddingTop: '30px'
+  };
+  const siderStyle = {
+    textAlign: 'center',
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: 'white',
+    paddingTop: '20px'
+  };
+  const footerStyle = {
+    textAlign: 'center',
+    color: '#fff',
+    backgroundColor: '#4096ff',
+  };
+  const questionsStyle = {
+
+  };
+  const titleOneStyle = {
+    marginTop: '20px', 
+    fontSize: '50px' ,
+    fontWeight: 'bold'
+  };
+  const titleTwoStyle = {
+    marginTop: '5px', 
+    fontSize: '40px' 
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ minHeight: "100vh" }}>
+
+      <Header style={headerStyle}>
+
+
+        <Layout>
+          <Sider width="20%" style={siderStyle}>
+            <Image
+              width={200}
+              src={logo}
+            />
+          </Sider>
+          <Content style={headerRightStyle}>
+            <Title level={1} style={titleOneStyle}>Wiki Trivial</Title>
+            <Title level={2} style={titleTwoStyle}>Juego de preguntas y respuestas</Title>
+          </Content>
+        </Layout>
+
+         
+          
+      </Header>
+      <Content style={contentStyle}>Content</Content>
+
+      <Footer style={footerStyle}>Wiki Trivial</Footer>    
+    </Layout>
   );
 }
 
