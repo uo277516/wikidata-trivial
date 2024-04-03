3fdeea56ce9e1a9f9d605821381dc802fa1995bf
uo277516 <90771958+uo277516@users.noreply.github.com>
2024-04-03 23:37:36 +0200

    "avanzando" con el front

--- front-trivial/src/App.js
+++ front-trivial/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import {Layout, Flex, Typography, Image} from 'antd';
 import logo from './logo.png'; 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 let App = () => {
 
 
-  const {Text, Title} = Typography;
+  const {Text, Title, Paragraph, Link} = Typography;
   const { Header, Footer, Sider, Content } = Layout;
 
   const headerStyle = {
@@ -24,7 +24,7 @@ let App = () => {
     minHeight: 120,
     lineHeight: '120px',
     color: 'black',
-    backgroundColor: '#B0E7F8',
+    backgroundColor: 'white', //color de fondo de donde tengo todo lo que ye la app
   };
   const headerRightStyle = {
     textAlign: 'left',
@@ -61,6 +61,42 @@ let App = () => {
   };
 
 
+  const [investigatorLabels, setInvestigatorLabels] = useState([]);
+
+
+  const fetchData = async () => {
+    try {
+      let response = await fetch(
+        process.env.REACT_APP_BACKEND_BASE_URL + "/P19",
+        {
+            method: "GET"
+        });
+
+      if (response.ok) {
+          let jsonData = await response.json();
+          const labels = jsonData.map(item => item.investigatorLabel);
+          setInvestigatorLabels(labels);
+          console.log(jsonData);
+      } else {
+          let responseBody = await response.json();
+          let serverErrors = responseBody.errors;
+          serverErrors.forEach(e => {
+              console.log("Error: " + e.msg)
+          })
+      }
+    } catch (error) {
+        console.error("Error fetching data:",error);
+    }
+    };
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+
+
+
   return (
     <Layout style={{ minHeight: "100vh" }}>
 
@@ -83,7 +119,33 @@ let App = () => {
          
           
       </Header>
-      <Content style={contentStyle}>Content</Content>
+      <Content style={contentStyle}>
+
+        <Layout>
+          <Content width="60%" style={contentStyle}>   {/*para poner las preguntas y eso*/}
+          
+            <Paragraph style={{fontSize:"20px"}}>
+              La información de las siguientes preguntas se ha recogido de 
+              <Link href="https://www.wikidata.org/?uselang=es" target="_blank" style={{fontSize:"20px"}}> Wikidata. </Link>
+              Las respuestas que usted proporcione se utilizarán para enriquecer la misma.
+            </Paragraph>
+
+            <Content style={{ textAlign: 'left', paddingLeft: '20px', minHeight: 120, lineHeight: '120px', color: 'black', backgroundColor: 'white' }}>
+              {investigatorLabels.map((label, index) => (
+                <Paragraph key={index}>{label}</Paragraph>
+              ))}
+            </Content>
+
+          
+          </Content>
+          <Sider width="40%" style={siderStyle}>      {/*para poner lo de la racha*/}
+            
+          </Sider>
+        </Layout>
+
+        
+
+      </Content>
 
       <Footer style={footerStyle}>Wiki Trivial</Footer>    
     </Layout>
