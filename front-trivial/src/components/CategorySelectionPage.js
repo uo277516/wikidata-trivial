3b802d41fd3c616783bdb4fb6cb641b2485572eb
uo277516 <90771958+uo277516@users.noreply.github.com>
2024-04-25 04:11:30 +0200

    hecha pantalla seleccionar categoria principal

--- front-trivial/src/components/CategorySelectionPage.js
+++ front-trivial/src/components/CategorySelectionPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Layout, Typography, Radio, Button, Modal } from 'antd';
-import { headerStyle, contentStyle, footerStyle } from '../styles/appStyle.js';
+import { Layout, Typography, Radio, Button, Modal, Image } from 'antd';
+import { headerStyle, contentStyle, footerStyle, headerRightStyle, siderStyle, titleOneStyle, titleTwoStyle} from '../styles/appStyle.js';
 import { useNavigate } from 'react-router-dom';
+import logo from '../logo.png'; 
 
 
-const { Title, Paragraph } = Typography;
-const { Header, Content, Footer } = Layout;
+const { Title, Paragraph, Link} = Typography;
+const { Header, Content, Footer, Sider } = Layout;
 
 const CategorySelectionPage = () => {
   const navigate = useNavigate();
@@ -32,31 +33,48 @@ const CategorySelectionPage = () => {
   return (
     <Layout style={{ minHeight: "100vh" }}>
       <Header style={headerStyle}>
-        <Title level={1} style={{ color: 'white', margin: '0 auto' }}>Selección de Categoría</Title>
+      <Layout>
+          <Sider width="20%" style={siderStyle}>
+            <Image
+              width={200}
+              src={logo}
+            />
+          </Sider>
+          <Content style={headerRightStyle}>
+            <Title level={1} style={titleOneStyle}>Wiki Trivial</Title>
+            <Title level={2} style={titleTwoStyle}>Juego de preguntas y respuestas</Title>
+          </Content>
+        </Layout>
       </Header>
       <Content style={contentStyle}>
+      <Title level={1} style={{ color: '#004aad', margin: '0 auto' }}>Selección de Categoría</Title>
+      <Paragraph style={{ fontSize: "20px", marginTop: '20px' }}>
+                  La información de las siguientes preguntas se ha recogido de 
+                  <Link href="https://www.wikidata.org/?uselang=es" target="_blank" style={{fontSize:"20px"}}> Wikidata. </Link>
+                  Las respuestas que usted proporcione se utilizarán para enriquecer la misma. Selecciona la categoría con la que quieres jugar.
+                </Paragraph>
         <Content width="100%" style={contentStyle}>
-          <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <Radio.Group 
-              value={selectedCategory} 
-              onChange={(e) => handleCategoryChange(e)}>
-              {categories.map((category) => (
-                <Radio.Button key={category} value={category}>
-                  {category}
-                </Radio.Button>
-              ))}
-            </Radio.Group>
-            <Paragraph style={{ fontSize: "20px", marginTop: '20px' }}>
-              Selecciona la categoría con la que quieres jugar.
-            </Paragraph>
-            <Button 
-              type="primary" 
-              size="large" 
-              onClick={() => handleStartGame(selectedCategory)} 
-              style={{ marginTop: '50px' }}>
-              Comenzar juego
-            </Button>
-          </div>
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+          <Radio.Group 
+            value={selectedCategory} 
+            onChange={(e) => handleCategoryChange(e)}
+            style={{ display: 'block' }}> 
+            {categories.map((category) => (
+              <Radio.Button key={category} value={category} 
+                  style={{ display: 'block', width: '40%', margin: '0 auto 10px' }}> 
+                {category}
+              </Radio.Button>
+            ))}
+          </Radio.Group>
+          <Button 
+            type="primary" 
+            size="large" 
+            onClick={() => handleStartGame(selectedCategory)} 
+            style={{ marginTop: '20px' }}> 
+            Comenzar juego
+          </Button>
+        </div>
+
         </Content>
       </Content>
       <Footer style={footerStyle}>Wiki Trivial</Footer>    
