51e9f4340143433ebe23152379d04a2330b59c28
uo277516 <90771958+uo277516@users.noreply.github.com>
2024-05-01 18:19:51 +0200

    avances? creo, no está como es pero

---
+++ back-trivial/config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+config.session_secret = "natalia2001semellones";
+config.consumer_key = "9710c01964941c18292d69c3cd033af2";
+config.consumer_secret = "7d61936d0335c9443f3a041812a25bc948ef29b8";
+
+module.exports = config;
