08ec48e10e0fe5bfbe1fbfc6c94a6a946d5ff4c3
uo277516 <90771958+uo277516@users.noreply.github.com>
2024-05-16 00:57:10 +0200

    imagen resonsive? no estoy segura

--- front-trivial/src/components/QuestionCard.js
+++ front-trivial/src/components/QuestionCard.js
@@ -7,49 +7,61 @@ const {Title, Paragraph, Link} = Typography;
 
 
 let QuestionCard = (props) => {
+
     let {imagenUrl, questionSelected, entity} = props;
 
     const cardStyle = {
-        width: '100%', // Expande la tarjeta para que ocupe todo el ancho disponible
-    };
+    maxWidth: '100%', 
+    width: '80vw',
+    height: '40vh'
+  };
 
-    const imgStyle = {
-        width: '100%', // Ancho completo de la imagen
-        height: '200px', // Altura fija para crear una imagen cuadrada
-        objectFit: 'cover', // Recorta la imagen para que se ajuste al contenedor manteniendo las proporciones
-    };
+  const imgStyle = {
+    width: '30vw',
+    height:' 40vh',
+    objectFit:'cover'
+  };
+
+ 
 
     const error_url = vars.fallback;
       
     const url = getSitelinkUrl({ site: 'wikidata', title: entity });
 
     return (
-        <>
-            <Card hoverable style={cardStyle}>
-                <div style={{ position: 'relative', width: '100%', height: '200px' }}>
+    <>
+        <Card hoverable style={cardStyle} styles={{ body: { padding: 0, overflow: 'hidden' } }}>
+            <Flex>
+                <div>
                     {imagenUrl ? (
-                        <img
-                            alt="Imagen"
-                            src={imagenUrl}
-                            style={imgStyle}
-                        />
+                    <img
+                        alt="Imagen"
+                        width={200}
+                        src={imagenUrl}
+                        style={imgStyle}
+                    />
                     ) : (
-                        <img
-                            alt="Imagen no disponible"
-                            src={error_url} 
-                            style={imgStyle}
-                        />
+                    <img
+                        alt="Imagen no disponible"
+                        width={200}
+                        src={error_url} 
+                        style={imgStyle}
+                    />
                     )}
                 </div>
-                <div style={{ padding: '16px' }}>
-                    <Title level={3}>{questionSelected}</Title>
+                <Flex vertical align="flex-end" justify="space-between" style={{ padding: 32 }}>
+                    <Title level={3} style={{ fontSize: '20px', marginBottom: '25px', marginTop: '50px' }}>
+                        {questionSelected}
+                    </Title>
                     <Paragraph>
                         Para más información, puede consultar su entrada en Wikidata en este 
-                        <Link href={url} target="_blank"> link. </Link>
+                        <Link href={url} target="_blank" style={{fontSize:"20px"}}> link. </Link>
                     </Paragraph>
-                </div>
-            </Card>
-        </>
+                </Flex>
+            </Flex>
+        </Card>
+
+    </>
     );
 };
 
